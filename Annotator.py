969134a3397a00e524620a1a82997c8b5f088ee5
aebenbach <andrewebenbach@gmail.com>
2025-04-28 18:43:58 -0400

    customizing annotate

--- Annotator.py
+++ Annotator.py
@@ -8,9 +8,19 @@ class Annotator:
         return self.annotate(im)
     
     def annotate(self, im):
-        results = self.model(im)
+        results = self.model(im)  # YOLO model inference
+        boxes = results[0].boxes  # Get bounding boxes
 
-        annotated_frame = results[0].plot() 
+        human_boxes = boxes[boxes.cls == 0]
+
+        results[0].boxes = human_boxes
+
+        annotated_frame = results[0].plot(
+                conf=False,         
+                labels=False,       
+                boxes=True,        
+                probs=False,
+        )
 
         return annotated_frame
 
